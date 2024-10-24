edec9cc15e74d713e1f61701972362901a07aa51
deepanshigautam <gdeepanshi7505@gmail.com>
2024-10-24 13:12:20 +0530

    Main card added

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
@@ -15,6 +16,60 @@ class WeatherScreen extends StatelessWidget {
         actions: [
          IconButton(onPressed: (){}, icon: const Icon(Icons.refresh)),
         ],
+      ),
+      body: Padding(
+        padding:const  EdgeInsets.all(16.0),
+      
+        child: Column(children: [
+        
+          //main card
+            SizedBox(
+            width: double.infinity,
+             child: Card(
+              elevation: 10,
+              shape:RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child:ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: BackdropFilter(
+                  filter:ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                  child: const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(children: [
+                      Text('300 k', style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      
+                      ),
+                      SizedBox(height:15),
+                      Icon(Icons.cloud, size: 64),
+                      SizedBox(height:15),
+                      Text('Rain', style:TextStyle(
+                        fontSize:20,
+                      ))
+                    ],),
+                  ),
+                ),
+              )
+             ),
+           ),
+           const SizedBox(height: 20),
+        
+        
+          //weather forecast card
+           const Placeholder(
+            fallbackHeight: 150,
+          ),
+         const SizedBox(height: 20),
+        
+           //additional info
+        const Placeholder(
+            fallbackHeight: 150,
+          ),
+        
+        ],),
       )
     );
   }
